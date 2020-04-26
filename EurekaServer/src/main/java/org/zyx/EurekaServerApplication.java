f0095e491c0d2baf2108fb2cdb3055c6de695508
刈剑丶 <14796682966@163.com>
2020-04-26 21:47:35 +0800

    SpringCloud外卖订餐平台启动!

---
+++ EurekaServer/src/main/java/org/zyx/EurekaServerApplication.java
@@ -0,0 +1,17 @@
+package org.zyx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by SunShine on 2020/4/26.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class,args);
+    }
+}
